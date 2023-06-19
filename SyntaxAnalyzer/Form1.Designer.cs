9b2dc40ee321800a5898b24a692bf879f2e761da
Augusto.Klein <augusto_klein@hotmail.com>
2023-06-18 22:05:01 -0300

    Adicao de componente de texto em tela

--- SyntaxAnalyzer/Form1.Designer.cs
+++ SyntaxAnalyzer/Form1.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnImport = new System.Windows.Forms.Button();
             this.btnRun = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // btnImport
@@ -56,11 +57,20 @@
             this.btnRun.UseVisualStyleBackColor = true;
             this.btnRun.Click += new System.EventHandler(this.button1_Click);
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 67);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(445, 506);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(469, 585);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.btnImport);
             this.Controls.Add(this.btnRun);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -74,6 +84,7 @@
 
         private System.Windows.Forms.Button btnRun;
         private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
