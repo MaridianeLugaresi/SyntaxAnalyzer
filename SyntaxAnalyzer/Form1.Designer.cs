e5dd832bfe95e138c3a1caa010f49e347edc1e96
Augusto.Klein <augusto_klein@hotmail.com>
2023-06-19 21:40:40 -0300

    Adicao de procedimentos para importar arquivo de codigo fonte no programa

--- SyntaxAnalyzer/Form1.Designer.cs
+++ SyntaxAnalyzer/Form1.Designer.cs
@@ -31,7 +31,8 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnImport = new System.Windows.Forms.Button();
             this.btnRun = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextCode = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
             // btnImport
@@ -44,6 +45,7 @@
             this.btnImport.TabIndex = 1;
             this.btnImport.Text = "Importar Arquivo";
             this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
             // 
             // btnRun
             // 
@@ -57,20 +59,27 @@
             this.btnRun.UseVisualStyleBackColor = true;
             this.btnRun.Click += new System.EventHandler(this.button1_Click);
             // 
-            // richTextBox1
+            // richTextCode
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 67);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(445, 506);
-            this.richTextBox1.TabIndex = 2;
-            this.richTextBox1.Text = "";
+            this.richTextCode.Location = new System.Drawing.Point(12, 67);
+            this.richTextCode.Name = "richTextCode";
+            this.richTextCode.Size = new System.Drawing.Size(445, 506);
+            this.richTextCode.TabIndex = 2;
+            this.richTextCode.Text = "";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.DefaultExt = "c";
+            this.openFileDialog.FileName = "openFD";
+            this.openFileDialog.InitialDirectory = "@C:\\";
+            this.openFileDialog.Title = "Localizar Arquivo";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(469, 585);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.richTextCode);
             this.Controls.Add(this.btnImport);
             this.Controls.Add(this.btnRun);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -84,7 +93,8 @@
 
         private System.Windows.Forms.Button btnRun;
         private System.Windows.Forms.Button btnImport;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextCode;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
     }
 }
 
--- SyntaxAnalyzer/Form1.cs
+++ SyntaxAnalyzer/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,34 @@ namespace SyntaxAnalyzer
 
         private void button1_Click(object sender, EventArgs e)
         {
+            
+        }
 
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = this.openFileDialog.ShowDialog();
+
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                richTextCode.Clear();
+
+                StreamReader sr = null;
+
+                try
+                {
+                    sr = File.OpenText(openFileDialog.FileName);
+                    string line;
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        richTextCode.AppendText(Environment.NewLine + line);
+                    }
+                }
+                catch (IOException exception)
+                {
+                    MessageBox.Show(exception.Message);
+                }
+            }
         }
     }
 }
